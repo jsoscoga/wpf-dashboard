8c17038e2a8953d877780077f73c9198c542dd24
jsoscoga <jsos_co_gar@hotmail.com>
2021-09-14 11:33:36 -0500

    -- Added namespace Views -- Added View StationStateView

---
+++ Views/StationStateView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dashboard.Views
+{
+    /// <summary>
+    /// Interaction logic for StationStateView.xaml
+    /// </summary>
+    public partial class StationStateView : UserControl
+    {
+        public StationStateView(string station, bool topVisibility, bool centerVisibility, bool bottomVisibility)
+        {
+            InitializeComponent();
+
+            DataContext = new
+            {
+                Station = station,
+                TopVisibility = SetVisibility(topVisibility),
+                CenterVisibility = SetVisibility(centerVisibility),
+                BottomVisibility = SetVisibility(bottomVisibility)
+            };
+        }
+
+        public Visibility SetVisibility(bool isVisible)
+        {
+            return isVisible ? Visibility.Visible : Visibility.Hidden;
+        }
+    }
+}
